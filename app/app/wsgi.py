cf91457274bd7d1c482db14e2a7db72f8b0f9600
Nermin Ayman <45697519+nerminaymann@users.noreply.github.com>
2024-07-08 16:31:58 +0300

    created django project via docker-compose

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Recipe_App project.
+WSGI config for app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recipe_App.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
