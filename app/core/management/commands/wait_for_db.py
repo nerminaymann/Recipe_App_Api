d58342af734a0251e8c01994d63f815bbd4dc3e7
Nermin Ayman <45697519+nerminaymann@users.noreply.github.com>
2024-07-27 19:29:58 +0300

    Add (wait_for_db) command for checking db is ready

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,9 +1,23 @@
 # Dajngo Command to wait for DB to be ready
 
+import time
+from psycopg2 import OperationalError as Psycopg20pError
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
 
     #django command to wait for db
     def handle(self, *args, **options):
-        pass
+        self.stdout.write('Waiting for database... ')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg20pError, OperationalError):
+                self.stdout.write('Database is not available yet, wait for 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database is Available!'))
