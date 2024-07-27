38fcd0cb513b6be6fbf8994273c9bae50dbba75d
Nermin Ayman <45697519+nerminaymann@users.noreply.github.com>
2024-07-27 18:55:49 +0300

    setup Custom Management Commands, Add Test for (wait_for_db) command TDD

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,9 @@
+# Dajngo Command to wait for DB to be ready
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    #django command to wait for db
+    def handle(self, *args, **options):
+        pass
