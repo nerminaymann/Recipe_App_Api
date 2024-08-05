1ca29b3d16c5b17d55ea6f95350715e674bc1dcd
Nermin Ayman <45697519+nerminaymann@users.noreply.github.com>
2024-08-05 03:57:03 +0300

    add Custom User & Custom User Manager

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+# from phonenumber_field.modelfields import PhoneNumberField
+from.managers import CustomUserManager
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(_("email address"),max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    # phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
+
+
+    def __str__(self):
+        return self.email
