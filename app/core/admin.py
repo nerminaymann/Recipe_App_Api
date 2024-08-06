51ecbeade755a7d5f64ac31a5466704461fac379
Nermin Ayman <45697519+nerminaymann@users.noreply.github.com>
2024-08-06 22:08:08 +0300

    customized the user Change Page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,11 +1,34 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from django.utils.translation import gettext_lazy as _
 
 class UserAdmin(BaseUserAdmin):
     #define the admin page for the users
     ordering = ['id']
     list_display = ['email','name']
+
+    fieldsets = (
+        (None, {'fields':('email','password')}),
+        (
+            _('Permissions'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields':('last_login',)
+            }
+        )
+    )
+    readonly_fields = ['last_login']
     
 admin.site.register(User,UserAdmin)
+
 
