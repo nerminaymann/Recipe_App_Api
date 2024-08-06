1917de32de1bea689e258816b559f5962c216c72
Nermin Ayman <45697519+nerminaymann@users.noreply.github.com>
2024-08-06 22:57:48 +0300

    add create new user page in admin testing and functionality

--- app/core/admin.py
+++ app/core/admin.py
@@ -28,6 +28,22 @@ class UserAdmin(BaseUserAdmin):
         )
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (
+            None, {
+                'classes': ('wide',),
+                'fields': (
+                    'email',
+                    'password1',
+                    'password2',
+                    'name',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+    )
     
 admin.site.register(User,UserAdmin)
 
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,8 +23,16 @@ class AdminSiteTests(TestCase):
         self.assertContains(res,self.user.name)
 
     def test_edit_user_page(self):
-
+        #test the edit user page works
         url = reverse('admin:core_user_change',args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code,200)
+
+    def test_create_user_page(self):
+        #test the create user page works
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code,200)
+
